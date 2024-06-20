b74c46453197fdbc430bde86e0e27cfafb92b3bd
Sam Vente <savente93@gmail.com>
2024-06-20 09:11:53 +0200

    initial setup
    remove workflows that ar ecurrently not needed
    
    add precomit file
    
    squash commit or working shared test cache
    
    Squash commit of loading mamba from cache env too
    
    squash commit of including py byte code in cache
    
    add dumy docs for testing
    
    turn off tests and enable docs
    
    add docs workflow
    
    tick
    
    squash commit of caching doc generation
    
    start new testing round for windows
    
    enable tmate debugging
    
    update names
    
    tick
    
    update action
    
    fix concurency group
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick
    
    tick

---
+++ playground/__init__.py
@@ -0,0 +1,3 @@
+"""It is MY PLAYGROUND. and I'll decide what to do with it."""
+
+GREETING = "Greetings stranger!"
---
+++ playground/math.py
@@ -0,0 +1,13 @@
+"""No time for docstrings, maths to be done."""
+
+
+def add_one(x):
+    """Add one to the input."""
+    return x + 1
+
+
+def rases_error():
+    """Raise an error."""
+    raise RuntimeError(
+        "Nice tests you got there. would be a shame if they would start failing."
+    )
